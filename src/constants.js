9245dda0f4bd02f6943869171f79dc40933168a0
lisette <84044778+lisette926@users.noreply.github.com>
2024-07-02 00:52:57 +0000

    fixed metric detail pages to take data from server

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const METRICS_MAP = {
+  pH: "pH",
+  "Oxygen Level": "oxygenLevel",
+  Salinity: "salinity",
+  Turbidity: "turbidity",
+  TDS: "tds",
+  Temperature: "temperature",
+};
--- src/routes/MetricDetail.jsx
+++ src/routes/MetricDetail.jsx
@@ -1,50 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Papa from "papaparse";
 import Chart from "react-apexcharts";
 import "./MetricDetail.css";
+import axios from "axios";
+import { METRICS_MAP } from "../constants";
 
 const googleDriveCsvUrl =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSRWgtxHG2hLyrOTSPxgKrMzhGlkcqdyLV8tQobZa8B4Pah4giCjY-Ek5euXmK5hvxf3vvPLZTHfzJG/pub?gid=156216978&single=true&output=csv";
 
-const dataTestArduino =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vR2WvNSmE8sFCEviG3keE78vd0vCAJJp5bC7NrV8LHCpyKr_8rcax51IpHHbUClx6bjAEeAeNYnQkrk/pub?output=csv";
+const serverData =
+  "https://smart-seawall-server-4c5cb6fd8f61.herokuapp.com/api/data";
 
 const MetricDetail = () => {
   const { metricName } = useParams();
   const [metricData, setMetricData] = useState(null);
 
   useEffect(() => {
-    const fetchDetailedMetricData = async (metricName) => {
-      Papa.parse(googleDriveCsvUrl, {
-        download: true,
-        header: true,
-        complete: function (results) {
-          const data = results.data;
-          const metricValues = data.map((row) => parseFloat(row[metricName]));
+    const fetchDetailedMetricData = async () => {
+      try {
+        const response = await axios.get(serverData);
+        const data = response.data;
 
-          // Compute current, average, highest, and lowest values
-          const avg = (
-            metricValues.reduce((sum, val) => sum + val, 0) /
-            metricValues.length
-          ).toFixed(2);
+        console.log("Fetched data:", data);
 
-          const highest = Math.max(...metricValues);
+        const metricKey = METRICS_MAP[metricName]; //new
+        const metricValues = data
+          .map((row) => parseFloat(row[metricKey]))
+          .filter((val) => !isNaN(val) && val !== null && val !== undefined);
 
-          const lowest = Math.min(...metricValues);
+        // Compute current, average, highest, and lowest values
+        const avg = (
+          metricValues.reduce((sum, val) => sum + val, 0) / metricValues.length
+        ).toFixed(2);
 
-          const latestValue = metricValues[metricValues.length - 1];
+        const highest = Math.max(...metricValues);
 
-          // Set the parsed data
-          setMetricData({
-            current: latestValue,
-            average: avg,
-            highest: highest,
-            lowest: lowest,
-            history: metricValues,
-          });
-        },
-      });
+        const lowest = Math.min(...metricValues);
+
+        const latestValue = metricValues[metricValues.length - 1];
+
+        // Set the parsed data
+        setMetricData({
+          current: latestValue,
+          average: avg,
+          highest: highest,
+          lowest: lowest,
+          history: metricValues,
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
 
     fetchDetailedMetricData(metricName);
